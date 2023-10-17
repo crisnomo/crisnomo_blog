58178a59babc67b18af1dadd18b6d642596d4034
crisnomo <cristina.s.f85@gmail.com>
2023-10-17 20:11:20 +0200

    funcional

--- main.py
+++ main.py
@@ -4,15 +4,68 @@ from web import form
 PORT = 8080
 
 web.config.debug = True
+
 render = web.template.render('templates')
 
-URLS = ("/", "index")
+URLS = ("/", "index",
+        "/lista", "lista",
+        "/articulo", "articulo",
+        "/sumar", "sumar",
+        "/restar", "restar")
 
 class index():
     
     def GET(self):
         return render.index()
     
+class restar():
+
+    def GET(self):
+        input=web.input()
+        id=input.id
+        db = web.database(dbn='mysql', host='192.168.1.100', db='produccion', user='remote', password='flores01')
+        data = db.select('produccion.publicaciones',where="idpublicacion = "+id,what="megusta")
+        megustaanterior = int(data[0]['megusta'])
+        db.update('produccion.publicaciones', where="idpublicacion = "+id, megusta = megustaanterior - 1)
+        data = db.select('produccion.publicaciones',where="idpublicacion = "+id)
+        data_list = list(data)
+        db.ctx.db.close()
+        return render.articulo(data_list)
+    
+    
+class sumar():
+
+    def GET(self):
+        input=web.input()
+        id=input.id
+        db = web.database(dbn='mysql', host='192.168.1.100', db='produccion', user='remote', password='flores01')
+        data = db.select('produccion.publicaciones',where="idpublicacion = "+id,what="megusta")
+        megustaanterior = int(data[0]['megusta'])
+        db.update('produccion.publicaciones', where="idpublicacion = "+id, megusta = megustaanterior +1)
+        data = db.select('produccion.publicaciones',where="idpublicacion = "+id)
+        data_list = list(data)
+        db.ctx.db.close()
+        return render.articulo(data_list)
+
+class articulo():
+    
+    def GET(self):
+        input=web.input()
+        id=input.id
+        db = web.database(dbn='mysql', host='192.168.1.100', db='produccion', user='remote', password='flores01')
+        data = db.select('produccion.publicaciones',where="idpublicacion = "+id)
+        data_list = list(data)
+        db.ctx.db.close()
+        return render.articulo(data_list)
+
+class lista():
+
+    def GET(self):
+        db = web.database(dbn='mysql', host='192.168.1.100', db='produccion', user='remote', password='flores01')
+        data = db.select('produccion.publicaciones')
+        data_list = list(data)
+        db.ctx.db.close()
+        return render.lista(data_list) 
 
 
 app = web.application(URLS, globals())
---
+++ tests/select.sql
@@ -0,0 +1,6 @@
+select *
+ from produccion.publicaciones p
+ 
+
+ 
+ 
